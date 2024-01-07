239049786d255bdf814abb9dee371a110f49f4ec
Kaviilee <jiaweilee95@126.com>
2024-01-08 00:09:42 +0800

    perf: 更新渲染类

--- index.js
+++ index.js
@@ -1,43 +1,30 @@
 /**
  * @description: 骚话渲染
  * @author: Kaviilee
- * @version: 1.1.5
+ * @version: 1.2.6
  */
 
 import $ from "jquery";
 import JX3BOX from "@jx3box/jx3box-common/data/jx3box.json";
-import { flatMapDeep } from "lodash"
-const { __imgPath, __dataPath } = JX3BOX;
+import {
+    flatMapDeep
+} from "lodash"
+const {
+    __imgPath,
+    __dataPath
+} = JX3BOX;
 
 // 获取全部表情
-function getEmotion() {
-    try {
-        const emotion = sessionStorage.getItem("jx3_emotion");
-        if (emotion) {
-            return;
-        } else {
-            fetch(`${__dataPath}emotion/output/catalog.json`)
-                .then((response) => response.json())
-                .then((data) => {
-                    sessionStorage.setItem(
-                        "jx3_emotion",
-                        JSON.stringify(data)
-                    );
-                });
-        }
-    } catch (e) {
-        fetch(`${__dataPath}emotion/output/catalog.json`)
-            .then((response) => response.json())
-            .then((data) => {
-                sessionStorage.setItem(
-                    "jx3_emotion",
-                    JSON.stringify(data)
-                );
-            });
+async function getEmotion() {
+    const emotion = sessionStorage.getItem("jx3_emotion");
+    if (emotion) {
+        return JSON.parse(emotion);
+    } else {
+        const data = await fetch(`${__dataPath}emotion/output/catalog.json`).then((response) => response.json());
+        sessionStorage.setItem("jx3_emotion", JSON.stringify(data));
+        return data;
     }
 }
-
-getEmotion();
 
 class JX3_EMOTION {
     /**
@@ -47,66 +34,44 @@ class JX3_EMOTION {
         this._joke = $.trim(joke);
         this.emotionList = [];
         this.max = 0;
-
-        this.init();
-    }
-
-    // 初始化实例
-    init() {
-        this.code = this._renderHTML();
-
-        return;
     }
 
     // 获取表情
-    loadEmotionList() {
-        const emotions = sessionStorage.getItem("jx3_emotion");
-        if (emotions) {
-            return JSON.parse(emotions);
-        }
-        return [];
+    async loadEmotionList() {
+        const emotionList = await getEmotion();
+        this.emotionList = emotionList;
     }
 
-    _renderHTML() {
-        if (this.max >= 3) {
-            return this._joke;
-        }
-        
-        this.emotionList = this.loadEmotionList();
+    async _renderHTML() {
         let str = this._joke;
 
-        // 1. 没有表情符号，直接返回
+        // 如果没有表情符号，直接返回
         if (!str.includes("#")) return str;
 
-        const emotions = flatMapDeep(this.emotionList.map(item => item.items));
-        const emotionKeys = emotions.map(item => item.key);
+        for (let i = 0; i < 3; i++) {
 
-        // 2. 获取表情符号的位置并且替换为img标签
-        while (emotionKeys.some(key => str.includes(key))) {
+            await this.loadEmotionList();
 
-            const _index = emotionKeys.findIndex(key => str.includes(key));
+            const emotions = flatMapDeep(this.emotionList.map(item => item.items));
+            const emotionKeys = emotions.map(item => item.key);
 
-            const key = emotionKeys[_index].replace('#', '$');
+            // 获取表情符号的位置并且替换为img标签
+            while (emotionKeys.some(key => str.includes(key))) {
+                const _index = emotionKeys.findIndex(key => str.includes(key));
+                const key = emotionKeys[_index].replace('#', '$');
+                str = _index > -1 ? str.replace(emotionKeys[_index], key) : str;
+            }
+            // 替换表情符号为img标签
+            while (emotionKeys.some(key => str.includes(`${key.replace('#', '$')}`))) {
+                const _index = emotionKeys.findIndex(key => str.includes(key.replace('#', '$')));
+                const _emotion = emotions.find(item => item.key === emotionKeys[_index]);
+                const src = _emotion ? `${__imgPath}emotion/output/${_emotion.filename}` : '';
+                const key = emotionKeys[_index].replace('#', '$');
+                str = src ? str.replace(key, `<img src="${src}" alt="${emotionKeys[_index]}" title="${emotionKeys[_index]}" />`) : str;
+            }
 
-            str = _index > -1 ? str.replace(emotionKeys[_index], key) : str;
-        }
-        // 3. 替换表情符号为img标签
-        while (emotionKeys.some(key => str.includes(`${key.replace('#', '$')}`))) {
-
-            const _index = emotionKeys.findIndex(key => str.includes(key.replace('#', '$')));
-
-            const _emotion = emotions.find(item => item.key === emotionKeys[_index]);
-
-            const src = _emotion ? `${__imgPath}emotion/output/${_emotion.filename}` : '';
-
-            const key = emotionKeys[_index].replace('#', '$');
-
-            str = src ? str.replace(key, `<img src="${src}" alt="${emotionKeys[_index]}" title="${emotionKeys[_index]}" />`) : str;
-        }
-        // 4. 递归 3 次，防止没获取到表情
-        if (!this.emotionList.length && this.max < 3) {
-            this._renderHTML();
-            this.max++;
+            // 如果已经获取到表情列表，就跳出循环
+            if (this.emotionList.length) break;
         }
 
         return str;
